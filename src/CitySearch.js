59d28fe8266750996bc24a2310671f1c79a97315
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-03-28 08:42:07 +0200

    4.4 Added initial tests for event search

---
+++ src/CitySearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+  render() {
+    return (
+      <div classname="CitySearch">
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
